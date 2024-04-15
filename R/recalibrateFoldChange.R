4b848cc88b9f6d9fc091c3ea81b641f7fb8c899c
Philipp Rentzsch <9343181+aerval@users.noreply.github.com>
2024-04-15 15:49:59 +0200

    fix vg assignment bug

--- R/recalibrateFoldChange.R
+++ R/recalibrateFoldChange.R
@@ -1,5 +1,5 @@
 #' @export
-recalibrateFoldChange <- function(df_de, tissue = "MEAN", vg = vg, remove_NA = FALSE,
+recalibrateFoldChange <- function(df_de, tissue = "MEAN", vg = vg_ae, remove_NA = FALSE,
                                   sort_by = NA, add_vg = FALSE, variance_offset = 0,
                                   FC_col_name = "log2FoldChange") {
 
@@ -13,7 +13,7 @@ recalibrateFoldChange <- function(df_de, tissue = "MEAN", vg = vg, remove_NA = F
   vg_select <- vg[row.names(df_de), tissue]
   sdg_select <- sqrt(vg_select + variance_offset)
 
-  df_de$recalibratedFC <- df_de[, FC_col_name] / sdg_select
+  df_de$recalibratedFC  <- df_de[, FC_col_name] / sdg_select
 
   if (add_vg) {
     df_de$vg <- vg_select
