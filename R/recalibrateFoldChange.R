bf912212c1e0743b7465273ff8b70d09a4e37951
Philipp Rentzsch <9343181+aerval@users.noreply.github.com>
2024-04-16 16:25:28 +0200

    added function documentation

--- R/recalibrateFoldChange.R
+++ R/recalibrateFoldChange.R
@@ -1,8 +1,37 @@
+#' Recalibrate expression fold changes
+#'
+#' @param df_de data.frame: contains differential expression per gene. Row names are
+#' Ensembl gene ids and one column contains the log fold change.
+#' @param vg data.frame: contains the V^G estimate per gene (rows) for one
+#' or multiple tissues (columns). Default = vg_ae, the allelic expression based
+#' V^G estimates calculated from GTEx v8.
+#' @param tissue char: V^G tissue that is recalibrated against. Default V^G are
+#' generated for the GTEx tissues (in GTex 6-letter code) or MEAN (weighted
+#' harmonic mean across tissues). Default = "MEAN".
+#' @param remove_NA bool: whether genes for which no V^G estimate exist should
+#' be removed from the final data.frame. Default = FALSE.
+#' @param sort_by char: sort result data.frame by one particular column.
+#' Default = NA
+#' @param add_vg bool: adds the V^G estimates used for recalibration to the
+#' result data.frame. Default = FALSE.
+#' @param variance_offset numeric: add an offset to all V^G estimates. Default
+#' = 0.
+#' @param FC_col_name char: column of df_de that contains the log fold change
+#' values that are recalibrated. Default = "log2FoldChange".
+#' @returns modified data.frame with added recalibrated fold changes.
+#' @examples
+#' df <- data.frame(log2FoldChange = c(-2.95, 1.03, 4.34),
+#'   padj = c(0, 1e-2, 1e-5),
+#'   row.names = c("ENSG00000000003", "ENSG00000000419", "ENSG00000000457"))
+#'
+#' recalibrateFoldChange(df)
+#' recalibrateFoldChange(df, tissue = "NERVET", vg = vg_aeml)
+#' recalibrateFoldChange(df, sort_by = "padj", add_vg = TRUE)
 #' @export
-recalibrateFoldChange <- function(df_de, tissue = "MEAN", vg = "vg_ae", remove_NA = FALSE,
+recalibrateFoldChange <- function(df_de, vg = "vg_ae", tissue = "MEAN", remove_NA = FALSE,
                                   sort_by = NA, add_vg = FALSE, variance_offset = 0,
                                   FC_col_name = "log2FoldChange") {
-  if (vg == "vg_ae") {
+  if (is.character(vg) && vg == "vg_ae") {
     vg <- get("vg_ae") # via lazy-loading of the attached dataset
   }
   if (!is.element(tissue, colnames(vg))) {
